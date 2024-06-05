7212aa9030be06dab121bbade73a9dc89252ca5d
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-06-05 18:48:21 +0200

    implementation

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,17 +8,47 @@ import {
   Home,
   Settings,
 } from "./pages";
+import { getPath, type Path } from "./paths";
+import type { ReactNode } from "react";
 
-export const routes: RouteObject[] = [
+type RouteDefinition = {
+  path?: Path | "*";
+  element?: ReactNode;
+  index?: boolean;
+} & (
+  | { index: true; children?: undefined }
+  | { index?: false; children?: Array<RouteDefinition> }
+);
+
+const routesDefinitionToRoute = ({
+  path,
+  children,
+  element,
+  index,
+}: RouteDefinition): RouteObject => {
+  const routePath =
+    path && path !== "*" ? getPath(path) : path;
+
+  return index
+    ? { path: routePath, element, index }
+    : {
+        path: routePath,
+        element,
+        index,
+        children: children?.map(routesDefinitionToRoute),
+      };
+};
+
+const routesDefinition: RouteDefinition[] = [
   {
     element: <Layout />,
     children: [
-      { path: "/", element: <Home /> },
+      { path: "home", element: <Home /> },
       {
         path: "tools",
         children: [
-          { path: "rules", element: <Rules /> },
-          { path: "synonyms", element: <Synonyms /> },
+          { path: "tools.rules", element: <Rules /> },
+          { path: "tools.syno", element: <Synonyms /> },
           { index: true, element: <Tools /> },
         ],
       },
@@ -27,3 +57,6 @@ export const routes: RouteObject[] = [
     ],
   },
 ];
+export const routes: RouteObject[] = routesDefinition.map(
+  routesDefinitionToRoute
+);
